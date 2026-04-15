04f7b453b5e0227c34f7e745fcc1d452a9fc0094
Mrunal Patil <patilmrunal2197@gmail.com>
2026-04-15 14:38:59 +0530

    Trigger CI/CD workflow 2

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, Spring Boot POC!";
+        return "Hello, Spring Boot POC22!";
     }
 }
